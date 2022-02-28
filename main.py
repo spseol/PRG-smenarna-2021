f2bea821dd13fec639b1d0fd056b9de6ae292e30
Marrek Nožka <nozka@spseol.cz>
2022-02-28 09:38:46 +0100

    zacatek

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 
 from os.path import basename, splitext
 import tkinter as tk
+from tkinter import Listbox, END
 
 # from tkinter import ttk
 
@@ -32,6 +33,21 @@ class Application(tk.Tk):
         self.btn.pack()
         self.btn2 = tk.Button(self, text="About", command=self.about)
         self.btn2.pack()
+
+        self.lstBx = Listbox(self)
+        self.lstBx.pack()
+        self.lstBx.bind("<ButtonRelease-1>", self.kliknu)
+
+        f = open("listek.txt")
+        self.radky = f.readlines()
+
+        for radek in self.radky:
+            radek = radek.split()
+            self.lstBx.insert(END, radek[0])
+
+    def kliknu(self, event):
+        index = self.lstBx.curselection()[0]
+        print(self.radky[index])
 
     def about(self):
         window = About(self)
